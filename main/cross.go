0a3c59ddf7132878ffcb78c4576366b27de79490
syedyus <123729735+syedyus@users.noreply.github.com>
2023-02-10 17:19:29 +0000

    Initial commit

---
+++ main/cross.go
@@ -0,0 +1,25 @@
+embedded_components {
+  id: "cross"
+  type: "sprite"
+  data: "tile_set: \"/main/game.atlas\"\n"
+  "default_animation: \"Untitled-1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.15
+    y: 0.15
+    z: 1.0
+  }
+}
